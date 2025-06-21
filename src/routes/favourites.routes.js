671661a66b3da55d72e3c30be346463e0b78cdcb
dhiraj-001 <gogoidhiraj207@gmail.com>
2025-06-22 00:52:57 +0530

    favourites route

--- src/routes/favourites.routes.js
+++ src/routes/favourites.routes.js
@@ -4,7 +4,7 @@ import { addFavourite, deleteFavourite, getFavourites } from "../controllers/fav
 const router = Router()
 
 
-router.route("/getFavourites").post(getFavourites)
+router.route("/getFavourites/:userId").post(getFavourites)
 router.route("/favourite").post(addFavourite)
 router.route("/delete/:userId/:recipeId").delete(deleteFavourite)
 
