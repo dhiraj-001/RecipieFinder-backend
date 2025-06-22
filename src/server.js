8d13ae9f037189ab23c4442bb01c35c40ac28d08
dhiraj-001 <gogoidhiraj207@gmail.com>
2025-06-22 09:03:47 +0530

    cron jobs added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import foodRouter from "./routes/favourites.routes.js"
+import job from "./config/cron.js";
 
 const app = express();
 app.use(express.json({limit: "200kb"}));
 app.use(express.urlencoded({extended: true, limit: "100kb"}));
 app.use(express.static("public"))
 const PORT = ENV.PORT;
+
+if(ENV.NODE_ENV == "production") job.start()
 
 app.use("/api/food", foodRouter)
 
